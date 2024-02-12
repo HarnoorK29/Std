ba075d42bf5a014378e44b7ad59f467295229ec1
HarnoorK29 <gill1014@sheridancollege.ca>
2024-02-12 15:07:17 -0500

    Revert "studentLISt changes done"
    This reverts commit 59abc2af4e746baa51dbd535530829d0a7e90a1b.

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
@@ -24,9 +24,7 @@ public class StudentList {
        {
            System.out.println(s.getName());
            
-           System..out.println("Hey ! I am Harnoor ");
-           System.out.println("I have made a  change :)");
-           
+           System..out.println("Hey ! I am Harnoor ")
        }
         
     }
